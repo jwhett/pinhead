bb047f5fb860c30019c045e7594c1fb2ad13df77
Josh Whetton <jwhett@protonmail.com>
2021-05-14 15:23:53 -0400

    Identify users with specific roles
    end goal is to deny/remove pin if a mod
    reacts with :no_entry_sign:

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ const auth = require('./auth.json');
 const config = require('./config.json');
 
 client.on('ready', () => {
-      console.log(`Logged in as ${client.user.tag}!`);
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', message => {
-    if (message.content === 'pinhead') {
-        message.channel.send("https://media.giphy.com/media/NsGsjTRJBj8Fq/giphy.gif");
-    }
+if (message.content === 'pinhead') {
+	message.channel.send("https://media.giphy.com/media/NsGsjTRJBj8Fq/giphy.gif");
+}
 });
 
 client.login(auth.token);
@@ -18,8 +18,14 @@ client.login(auth.token);
 client.on('messageReactionAdd', (mr, user) => {
 	// Don't care about bot reactions
 	if (user.bot) return;
-	console.log(`reaction from ${user.username}`);
+	if (mr.emoji.name === '🚫') { // block pinning
+		// get the guild member so we can check roles
+		const member = mr.message.guild.member(user);
+		if (member && member.roles.cache.find(role => role.name === config.ROLE)) {
+				console.log(`guild member is a mod`);
+		}
+	}
 	if (mr.emoji.name === '📌' && mr.message.pinnable && mr.count === config.MAX){
-            mr.message.pin();
-	};
+		mr.message.pin();
+	}
 });
